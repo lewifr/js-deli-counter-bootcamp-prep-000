9abed5ebecf9c3a456bbd3cf855d5d9c8c18a47b
Francis Lewis <francis.lewis@me.com>
2019-04-12 14:16:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function takeANumber(currentLine, name) {
   let position = currentLine.length
   return `Welcome, ${name}. You are number ${position} in line.`
 }
+
+function nowServing(currentLine) {
+  let firstPerson = currentLine[0]
+  currentLine.unshift()
+  return firstPerson
+}
